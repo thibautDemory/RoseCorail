1bbbb4dd827241447b6976c15184c15fc6cef60c
ThibautDEMORY <thibautdemory@hotmail.fr>
2018-01-31 16:25:11 +0100

    Ajout des servlets. mise à jour des liens dans le formulaire

---
+++ src/main/java/hei/ProjetRoseCorail/servlets/AjoutPanelColorisServlet.java
@@ -0,0 +1,22 @@
+package hei.ProjetRoseCorail.servlets;
+
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+@WebServlet("/ajoutPanelColoris")
+public class AjoutPanelColorisServlet extends GenericServlet{
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        WebContext webContext = new WebContext(req, resp, req.getServletContext());
+        TemplateEngine templateEngine = createTemplateEngine(req.getServletContext());
+
+        templateEngine.process("ajout-panel-coloris", webContext, resp.getWriter());
+    }
+}
