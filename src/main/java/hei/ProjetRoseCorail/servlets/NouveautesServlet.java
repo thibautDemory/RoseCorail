1151db93355384e94b4456f65e2cd139678ccab6
William <william.evrard@hei.yncrea.fr>
2018-03-19 12:16:16 +0100

    "Nouveautés" dynamisée

--- src/main/java/hei/ProjetRoseCorail/servlets/NouveautesServlet.java
+++ src/main/java/hei/ProjetRoseCorail/servlets/NouveautesServlet.java
@@ -1,5 +1,7 @@
 package hei.ProjetRoseCorail.servlets;
 
+import hei.ProjetRoseCorail.entities.Article;
+import hei.ProjetRoseCorail.managers.ArticleLibrary;
 import hei.ProjetRoseCorail.managers.CompteClientLibrary;
 import hei.ProjetRoseCorail.managers.CompteRoseCorailLibrary;
 import org.thymeleaf.TemplateEngine;
@@ -10,6 +12,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.*;
 
 @WebServlet("/nouveautes")
 public class NouveautesServlet extends GenericServlet {
@@ -20,6 +23,26 @@ public class NouveautesServlet extends GenericServlet {
         WebContext webContext = new WebContext(req, resp, req.getServletContext());
         TemplateEngine templateEngine = createTemplateEngine(req.getServletContext());
         String statut=(String) req.getSession().getAttribute("statut");
+
+
+        ArticleLibrary articleLibrary = ArticleLibrary.getInstance();
+        List<Article> listOfArticles =  articleLibrary.listarticles();
+        List<Article> listOf9LastArticles = new ArrayList<Article>();
+
+        int listSize =  listOfArticles.size();
+        if(listSize > 9){
+            for(int i=(listSize-1); i>=(listSize-9); i--){
+                listOf9LastArticles.add(listOfArticles.get(i));
+            }
+        }else{
+            for(int i=(listSize-1); i>=0; i--){
+                listOf9LastArticles.add(listOfArticles.get(i));
+            }
+        }
+        System.out.println("Size : "+listSize);
+        System.out.println("Real size : "+listOf9LastArticles.size());
+        webContext.setVariable("listOf9LastArticles",listOf9LastArticles);
+
 
         if (statut==null||"".equals(statut)||statut=="visiteur"){
             statut="visiteur";
